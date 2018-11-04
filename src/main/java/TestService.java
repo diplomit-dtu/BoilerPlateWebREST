821c66b3cca5b33055220d36f5ae0fe134305e3c
cbudtz <chrbudtz@gmail.com>
2018-11-04 10:18:19 +0100

    Initial commit

---
+++ src/main/java/TestService.java
@@ -0,0 +1,20 @@
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("test")
+public class TestService {
+
+    @GET
+    public String getTest(){
+        return "Hello World";
+    }
+
+    @Path("json")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Hello getHelloJson(){
+        return new Hello();
+    }
+}
